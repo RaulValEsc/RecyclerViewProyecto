7c776fd27f4b7aee84e13105ab36a63996e71049
RaulValEsc <raulvalesc@gmail.com>
2020-12-16 10:11:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewproyecto/Alumnos.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerviewproyecto
+
+class Alumnos(var dni: Int, var nombre: String, var apellidos: String, var imagen :String,var telefono :String,var tutor :String,var imagenTutor :String)
